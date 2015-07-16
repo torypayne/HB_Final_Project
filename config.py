2c551bc70318efa98f598842a8c745f29652023c
Victoria Payne <vhpayne@gmail.com>
2015-07-15 19:43:16 -0700

    Some database updates to make things still work

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 import os
 import datetime
 
-env = os.getenv('ENV', "local")
-if env == 'prod':
-	HOST = "us-cdbr-east-05.cleardb.net"
-	USER = "b64cd4ed64d781"
-	PASS = "eb2052db"
-	DB = "heroku_ff4277368662e82"
+# env = os.getenv('ENV', "local")
+# if env == 'prod':
+HOST = "us-cdbr-east-05.cleardb.net"
+USER = "b64cd4ed64d781"
+PASS = "eb2052db"
+DB = "heroku_ff4277368662e82"
 	# mysql://b64cd4ed64d781:eb2052db@us-cdbr-east-05.cleardb.net/heroku_ff4277368662e82?reconnect=true
-else:
-	HOST = "localhost"
-	USER = "root"
-	PASS = ""
-	DB = "hotelchains"
+# else:
+# 	HOST = "localhost"
+# 	USER = "root"
+# 	PASS = ""
+# 	DB = "hotelchains"
 
 
 DEFCHECKIN = "5/10/2014"
